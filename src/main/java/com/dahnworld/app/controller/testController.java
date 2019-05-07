aebdc4074fe4f521f09af92b35ac5694598cfc14
jes <hiter00@naver.com>
2019-05-07 17:50:07 +0900

    여기다가 하셈

--- src/main/java/com/dahnworld/app/controller/testController.java
+++ src/main/java/com/dahnworld/app/controller/testController.java
@@ -1,25 +1,22 @@
 package com.dahnworld.app.controller;
 
 import java.rmi.RemoteException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
+import org.tempuri.HSPMemberSoapProxy;
+import org.tempuri.MemberInfo;
 
 import com.dahnworld.app.dto.UserDto;
 import com.dahnworld.app.jwt.JwtProvider;
 import com.dahnworld.app.response.JwtResponse;
 import com.dahnworld.app.service.UserService;
-import com.dahnworld.app.util.DataTableUtil;
-
-import kr.co.br.dainnetuser.ws.wsMbrFeeGrade_asmx.GetMbrBranchGradeForMagoDbResponseGetMbrBranchGradeForMagoDbResult;
-import kr.co.br.dainnetuser.ws.wsMbrFeeGrade_asmx.WsMbrFeeGradeSoapProxy;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -42,22 +39,7 @@ public class testController {
 //		MemberInfo memberinfo = hsp.getTotaluser(testUserId);
 //		
 //		System.out.println("email : " + memberinfo.getEmail());
-		
-		//다인넷 실섭 웹서비스(메서드 호출 시 자기 것만 쓰기)
-//		WsMbrFeeGradeSoapProxy DainMember = new WsMbrFeeGradeSoapProxy();
 //		
-//		DataTableUtil dtu = new DataTableUtil();
-//		List<HashMap<String, String>> dataList = new ArrayList<HashMap<String, String>>();
-//		
-//		dtu.SetTable(DainMember.getMbrBranchGradeForMagoDb("안종한", "01043454142", "890211", "").get_any()[1].toString());
-//
-//		dataList = dtu.GetDataList();
-//		
-//		if(dataList.size() > 0) {
-//			System.out.println(dtu.GetKeyList());
-//			System.out.println("gradeCd : " + dataList.get(0).get("gradeCd"));
-//		}
-
 		String accessToken = jwtResponse.getToken();
 
 		if(accessToken == null) {
@@ -77,5 +59,10 @@ public class testController {
 		
 		
 		return ResponseEntity.ok(jwtResponse);
+	}
+	
+	@GetMapping("/ajs")
+	protected void ajs() throws RemoteException {
+		System.out.println("hh");
 	}
 }
